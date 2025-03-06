37483ce8d5b88e7ea9892167115f242e070553a4
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2025-03-06 19:37:08 +0100

    Solve issues linux release.

--- source/node_loader.c
+++ source/node_loader.c
@@ -4,7 +4,10 @@
 #include "libnode2.h"
 
 char *node_loader() {
+    printf("INSIDE NODE LOADER (before string_function call): %p\n", string_function);
+    fflush(stdout);
     char *str = string_function();
-    printf("INSIDE NODE LOADER: %s\n", str);
+    printf("INSIDE NODE LOADER (after): %s\n", str);
+    fflush(stdout);
     return str;
 }
